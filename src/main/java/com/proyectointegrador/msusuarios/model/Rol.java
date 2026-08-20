6a6b4470a198655f5b4a12622519edf753650d1d
mendezcarlos7 <mendezcarlos2028@gmail.com>
2026-08-20 18:25:12 -0500

    feat(usuarios): crear entidad Usuario con campos obligatorios [HU-01]

---
+++ src/main/java/com/proyectointegrador/msusuarios/model/Rol.java
@@ -0,0 +1,9 @@
+package com.proyectointegrador.msusuarios.model;
+
+public enum Rol {
+    ADMINISTRADOR,
+    PROPIETARIO,
+    EMPLEADO,
+    CLIENTE
+
+}
---
+++ src/main/java/com/proyectointegrador/msusuarios/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.proyectointegrador.msusuarios.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String documentoDeIdentidad;
+    private String celular;
+    private LocalDate fechaNacimiento;
+    private String correo;
+    private String clave;
+    private Rol rol;
+}
