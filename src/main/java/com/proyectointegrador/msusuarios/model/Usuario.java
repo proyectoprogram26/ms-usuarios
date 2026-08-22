3c975edf081d811832b5b29c1847a8a3aa199d85
Mrs-natiss <nathaliafanador13@gmail.com>
2026-08-22 15:03:45 -0500

    chore(usuarios): limpiar archivos duplicados y corregir ubicaciones [HU-05]

--- src/main/java/com/proyectointegrador/msusuarios/model/Usuario.java
+++ src/main/java/com/proyectointegrador/msusuarios/model/Usuario.java
@@ -1,6 +1,6 @@
 package com.proyectointegrador.msusuarios.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -32,9 +32,65 @@ public class Usuario {
 
     @Email
     private String correo;
-    @JsonIgnore
+    @JsonProperty
     private String clave;
     private Rol rol;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumentoDeIdentidad() {
+        return documentoDeIdentidad;
+    }
+
+    public void setDocumentoDeIdentidad(String documentoDeIdentidad) {
+        this.documentoDeIdentidad = documentoDeIdentidad;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 
     public String getClave() {
         return clave;
